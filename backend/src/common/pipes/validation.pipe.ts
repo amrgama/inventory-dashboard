b596eff0540aad26fb4c7000536f9487ae406c1c
Amr Jamal <amrj.bagha@gmail.com>
2026-03-08 21:37:18 +0200

    feat(backend): setup initial architecture and common infrastructure

---
+++ backend/src/common/pipes/validation.pipe.ts
@@ -0,0 +1,10 @@
+import { ValidationPipe as NestValidationPipe } from '@nestjs/common';
+
+export const ValidationPipe = new NestValidationPipe({
+  whitelist: true,
+  transform: true,
+  forbidNonWhitelisted: true,
+  transformOptions: {
+    enableImplicitConversion: true,
+  },
+});
